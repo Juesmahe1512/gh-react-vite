9be0c3ea5845970cbe3b9708122da8db442997eb
Juesmahe1512 <juesmahe@gmail.com>
2023-04-20 10:32:58 -0500

    FIX BUG

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://juesmahe1512.github.io/',
+  base: 'https://juesmahe1512.github.io/gh-react-vite',
   plugins: [react()],
 })
